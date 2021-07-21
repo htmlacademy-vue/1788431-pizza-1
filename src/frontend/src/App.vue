2b6ae42fda0ee6b9158055298a6a29fec53969f6
Volkov Sergey <sergeyv@hflabs.ru>
2021-07-21 22:37:28 +0300

    module2-task1 - create AppLayout

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,23 +1,6 @@
 <template>
   <div id="app">
-    <header class="header">
-      <div class="header__logo">
-        <a href="index.html" class="logo">
-          <img
-            src="@/assets/img/logo.svg"
-            alt="V!U!E! Pizza logo"
-            width="90"
-            height="40"
-          />
-        </a>
-      </div>
-      <div class="header__cart">
-        <a href="cart.html">0 ₽</a>
-      </div>
-      <div class="header__user">
-        <a href="#" class="header__login"><span>Войти</span></a>
-      </div>
-    </header>
+    <AppLayout></AppLayout>
 
     <Index></Index>
   </div>
@@ -25,10 +8,11 @@
 
 <script>
 import Index from "@/views/Index.vue";
+import AppLayout from "@/layouts/AppLayout.vue";
 
 export default {
   name: "App",
-  components: { Index },
+  components: { Index, AppLayout },
 };
 </script>
 
