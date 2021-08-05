3f26fffbe7fe447101bd523b2cd776c48fbd40f5
Volkov Sergey <sergeyv@hflabs.ru>
2021-08-05 22:50:27 +0300

    module3-task1 - fixes

--- src/frontend/src/layouts/AppLayoutUser.vue
+++ src/frontend/src/layouts/AppLayoutUser.vue
@@ -3,7 +3,14 @@
     <Header></Header>
     <main class="layout">
       <div class="layout__sidebar sidebar">
-        <div style="height: 90px"></div>
+        <a href="/" class="logo layout__logo">
+          <img
+            src="@/assets/img/logo.svg"
+            alt="V!U!E! Pizza logo"
+            width="90"
+            height="40"
+          />
+        </a>
 
         <a class="layout__link layout__link--active" href="/orders"
           >История заказов</a
