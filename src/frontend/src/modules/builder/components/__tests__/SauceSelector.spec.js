2d41264896195bf7eba8671f68a4df16f59bce70
Volkov Sergey <sergeyv@hflabs.ru>
2021-11-19 10:32:05 +0300

    module7-task1 SaucesSelector tests

--- src/frontend/src/modules/builder/components/__tests__/SauceSelector.spec.js
+++ src/frontend/src/modules/builder/components/__tests__/SauceSelector.spec.js
@@ -1,22 +1,22 @@
 import { shallowMount } from "@vue/test-utils";
-import DoughSelector from "@/modules/builder/components/DoughSelector.vue";
+import SauceSelector from "@/modules/builder/components/SauceSelector.vue";
 import { generateMockStore } from "@/store/mocks";
 import { fillPizzaData } from "@/common/testHelpers";
 import pizza from "@/static/pizza";
 
-describe("DoughSelector.vue", () => {
+describe("SauceSelector.vue", () => {
   let wrapper;
   let store;
   let actions;
 
   const createComponent = (options) => {
-    wrapper = shallowMount(DoughSelector, options);
+    wrapper = shallowMount(SauceSelector, options);
   };
 
   beforeEach(() => {
     actions = {
       Builder: {
-        saveDoughId: jest.fn(),
+        saveSauceId: jest.fn(),
       },
     };
     store = generateMockStore(actions);
@@ -32,28 +32,28 @@ describe("DoughSelector.vue", () => {
     expect(wrapper.exists()).toBeTruthy();
   });
 
-  it("should render doughs", () => {
+  it("should render sauces", () => {
     createComponent({ store });
-    expect(wrapper.findAll("input").length).toBe(pizza.dough.length);
+    expect(wrapper.findAll("input").length).toBe(pizza.sauces.length);
   });
 
-  it("should be selected default dough", () => {
+  it("should be selected default sauce", () => {
     createComponent({ store });
     expect(wrapper.findAll("input").at(0).element.checked).toBe(true);
     expect(wrapper.findAll("input").at(1).element.checked).toBe(false);
   });
 
-  it("should be selected right dough", () => {
-    store.commit("Builder/setDoughId", pizza.dough[1].id);
+  it("should be selected right sauce", () => {
+    store.commit("Builder/setSauceId", pizza.sauces[1].id);
     createComponent({ store });
     expect(wrapper.findAll("input").at(0).element.checked).toBe(false);
     expect(wrapper.findAll("input").at(1).element.checked).toBe(true);
   });
 
-  it("can select dough", () => {
+  it("can select sauce", () => {
     createComponent({ store });
     wrapper.findAll("input").at(1).trigger("click");
-    expect(actions.Builder.saveDoughId).toHaveBeenCalledWith(
+    expect(actions.Builder.saveSauceId).toHaveBeenCalledWith(
       expect.any(Object),
       2
     );
