107b1b017e4eb0cc238788f2583f0def6c247279
Volkov Sergey <sergeyv@hflabs.ru>
2021-11-21 20:39:55 +0300

    module7-task1 Order tests

--- src/frontend/src/common/testHelpers.js
+++ src/frontend/src/common/testHelpers.js
@@ -71,3 +71,56 @@ export const pizzaData2 = {
     ingredients: "грибы, чеддер",
   },
 };
+
+export const orderData1 = {
+  id: 1,
+  phone: "",
+  userId: "12e9c73e-7711-44d7-9546-4b59e9838120",
+  addressId: null,
+  address: "адрес",
+  orderPizzas: [
+    {
+      id: 1,
+      name: "pizza",
+      quantity: 1,
+      sauceId: 1,
+      doughId: 1,
+      sizeId: 1,
+      orderId: 1,
+      ingredients: [
+        { id: 1, quantity: 1, pizzaId: 1, ingredientId: 1 },
+        { id: 2, quantity: 2, pizzaId: 1, ingredientId: 2 },
+      ],
+      price: 467,
+      humanize: {
+        dough: "на тонком тесте",
+        sauce: "томатный",
+        size: "23 см",
+        ingredients: "грибы, чеддер",
+      },
+    },
+  ],
+  orderMisc: [
+    {
+      id: 1,
+      quantity: 1,
+      orderId: 1,
+      miscId: 1,
+      price: 56,
+      name: "Cola-Cola 0,5 литра",
+      code: "cola",
+      totalPrice: 56,
+    },
+  ],
+  price: 523,
+};
+
+export const orderData2 = {
+  id: 1,
+  phone: "",
+  userId: "12e9c73e-7711-44d7-9546-4b59e9838120",
+  addressId: null,
+  orderPizzas: [],
+  orderMisc: [],
+  price: 0,
+};
